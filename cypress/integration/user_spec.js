dec14a107ab770e80d2869f8e583ddb343d9ac8d
vicus <vasile.pop@gmail.com>
2018-01-26 22:18:01 +0200

    added faker package for data generation

--- cypress/integration/user_spec.js
+++ cypress/integration/user_spec.js
@@ -1,12 +1,18 @@
+const faker = require('faker')
+
 // Suita de teste va verifica functionalitati specifice userilor
 
 //SELECTORS
 const INPUT_EMAIL = 'input[name="email"]'
 const INPUT_PASSWORD = 'input[name="password"]'
+const INPUT_CONFIRM_PASSWORD = 'input[name="confirm_password"]'
+const INPUT_FIRST_NAME = 'input[name="first_name"]'
+const INPUT_LAST_NAME = 'input[name="last_name"]'
 const INPUT_SUBMIT = 'input[type="submit"]'
+const INPUT_RESET = 'input[type="reset"]'
 const ROOT_URL = 'http://mylibrary.test/src'
-const LOGIN_URL = ROOT_URL + 'login.php'
-const REGISTER_URL = ROOT_URL + 'register.php'
+const LOGIN_URL = ROOT_URL + '/login.php'
+const REGISTER_URL = ROOT_URL + '/register.php'
 
 describe('Authentication scenarios', function () {
     context('Login page', function () {
@@ -15,6 +21,7 @@ describe('Authentication scenarios', function () {
             cy.visit(LOGIN_URL)
             //validate page title
             cy.title().should('be', 'Login')
+            cy.contains('Sign up now')
         })
 
         it('should reject missing entries in login form', function () {
@@ -27,9 +34,63 @@ describe('Authentication scenarios', function () {
             cy.url(LOGIN_URL)
         })
 
-        it('should register new user', function () {
+    })
+
+    context('Registration page', function () {
+        beforeEach(function () {
+            //open registration page
+            cy.visit(LOGIN_URL)
             cy.contains('Sign up now').click()
+        })
+
+        it('should validate input fields', function () {
+            //validate url and page title
+            cy.title().should('be', 'My Library')
             cy.url(REGISTER_URL)
+            cy.contains('Please fill in your new account details')
+            cy.get(INPUT_SUBMIT).click()
+            cy.contains('Please enter an email.')
+            cy.contains('Please add first name.')
+            cy.contains('Please add last name.')
+            cy.contains('Please enter a password.')
+            cy.contains('Please confirm password.')
+        })
+
+        it('should reset form fields', function () {
+            //validate url
+            cy.url(REGISTER_URL)
+            //type values in every input fields
+            cy.get(INPUT_EMAIL).type("fake")
+            cy.get(INPUT_FIRST_NAME).type("fake")
+            cy.get(INPUT_LAST_NAME).type("fake")
+            cy.get(INPUT_PASSWORD).type("fake")
+            cy.get(INPUT_PASSWORD).type("fake")
+            //click Reset
+            cy.get(INPUT_RESET).click()
+            //all input fields should be empty
+            cy.get(INPUT_EMAIL).should('have.value', '')
+            cy.get(INPUT_FIRST_NAME).should('have.value', '')
+            cy.get(INPUT_LAST_NAME).should('have.value', '')
+            cy.get(INPUT_PASSWORD).should('have.value', '')
+            cy.get(INPUT_PASSWORD).should('have.value', '')
+        })
+
+        it('should create account', function () {
+            //validate url
+            cy.url(REGISTER_URL)
+            //generate user details
+            const firstname = faker.name.firstName()
+            const lastname = faker.name.lastName()
+            const email = firstname + '.' + lastname + faker.random.number() + '@my.library'
+            //type values in every input fields
+            cy.get(INPUT_EMAIL).type(email)
+            cy.get(INPUT_FIRST_NAME).type(firstname)
+            cy.get(INPUT_LAST_NAME).type(lastname)
+            cy.get(INPUT_PASSWORD).type("secret")
+            cy.get(INPUT_CONFIRM_PASSWORD).type("secret")
+            //click Submit
+            cy.get(INPUT_SUBMIT).click()
+
         })
 
     })
